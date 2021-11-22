a07646b9984191e43d74e1021eede473899c967c
Marcel G <arksol89@gmail.com>
2021-11-22 22:24:11 +0100

    Exercise 8,9,10,11 -> Added

--- Homework/L2C/01/homework.js
+++ Homework/L2C/01/homework.js
@@ -18,13 +18,17 @@ function start() {
 function mainMenu() {
   console.log("Where you want to go?");
   console.log("");
-  console.log("1. Exercise 1 - Input and output");
-  console.log("2. Exercise 2 - Format text");
-  console.log("3. Exercise 3 - Currency converter");
-  console.log("4. Exercise 4 - Sum loop");
-  console.log("5. Exercise 5 - Sum loop modified");
-  console.log("6. Exercise 6 - Sum loop modifed 2");
-  console.log("7. Exercise 7 - Beachwalk Hotel");
+  console.log("1.  Exercise 1  - Input and output");
+  console.log("2.  Exercise 2  - Format text");
+  console.log("3.  Exercise 3  - Currency converter");
+  console.log("4.  Exercise 4  - Sum loop");
+  console.log("5.  Exercise 5  - Sum loop modified");
+  console.log("6.  Exercise 6  - Sum loop modifed 2");
+  console.log("7.  Exercise 7  - Beachwalk Hotel");
+  console.log("8.  Exercise 8  - Check array for number");
+  console.log("9.  Exercise 9  - Odd positions");
+  console.log("10. Exercise 10 - Longest name");
+  console.log("11. Exercise 11 - Sum, avg, highest, lowest");
   console.log("");
 
   let route = readlineSync.question("> Number: ");
@@ -35,7 +39,7 @@ function mainMenu() {
   if (route == "1") {
     inputOutput();
   } else if (route == "2") {
-    mortelKombat();
+    mortalKombat();
   } else if (route == "3") {
     currencyConverter();
   } else if (route == "4") {
@@ -45,9 +49,16 @@ function mainMenu() {
   } else if (route == "6") {
     sumMix();
   } else if (route == "7") {
-    beackwalkHotel();
+    beachWalkHotel();
+  } else if (route == "8") {
+    checkIfNumberExists();
+  } else if (route == "9") {
+    printOddPosition();
+  } else if (route == "10") {
+    printLongestName();
+  } else if (route == "11") {
+    sumAvgHighestLowest();
   } else {
-
     console.log("Sorry " + username + " I couldn't understand :(");
     mainMenu();
   }
@@ -60,7 +71,7 @@ function inputOutput() {
 }
 
 //Exercise 2
-function mortelKombat() {
+function mortalKombat() {
   console.log("");
   console.log("                       _..gggggppppp.._                       ");
   console.log("                  _.gd$$$$$$$$$$$$$$$$$$bp._                  ");
@@ -81,7 +92,7 @@ function mortelKombat() {
   console.log(":$$$      $$$$$$$$$$$$$$$               T$$p._;$b         $$$;");
   console.log("$$$;      $$$$$$$$$$$$$$;                `T$$$$:Tb        :$$$");
   console.log("$$$;      $$$$$$$$$$$$$$$                        Tb    _  :$$$");
-  console.log(":$$$     d$$$$$$$$$$$$$$$.                        $b.__Tb $$$;");
+  console.log(":$$$     d$$$$$$$$$$$$$$$.                       $b.__Tb $$$;");
   console.log(":$$$  .g$$$$$$$$$$$$$$$$$$$p...______...gp._      :$`^^^' $$$;");
   console.log(" $$$;  `^^'T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$p.    Tb._, :$$$ ");
   console.log(" :$$$       T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$b.   ^^^  $$$; ");
@@ -89,7 +100,7 @@ function mortelKombat() {
   console.log("  :$$$        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;     $$$;  ");
   console.log("   T$$b    _  :$$`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$;   d$$P   ");
   console.log("    T$$b   T$g$$; :$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  d$$P    ");
-  console.log("     T$$b   `^^'  :$$  ^T$$$$$$$$$$$$$$$$$$$$$$$$$$$ d$$P     ");
+  console.log("     T$$b   `^^'  :$$  ^T$$$$$$$$$$$$$$$$$$$$$$$$$$$ d$$P     ")
   console.log("      T$$b        $P     T$$$$$$$$$$$$$$$$$$$$$$$$$;d$$P      ");
   console.log("       T$$b.      '       $$$$$$$$$$$$$$$$$$$$$$$$$$$$P       ");
   console.log("        `T$$$p.          d$$$$$$$$$$$$$$$$$$$$$$$$$$P'        ");
@@ -106,6 +117,7 @@ function currencyConverter() {
   console.log("Exercise 3 - Currency converter");
   console.log("");
   console.log("What amount of USD you want to convert to Euro, " + username + " ?");
+
   let dollar = readlineSync.question("> USD ");
   result = (dollar * 0.8863633).toFixed(2);
   console.log("");
@@ -231,7 +243,7 @@ function sumMixOption(num) {
 }
 
 //Exercise 7
-function beackwalkHotel() {
+function beachWalkHotel() {
 
   console.log("Exercise 7 - Beackwalk Hotel");
   console.log("");
@@ -252,7 +264,7 @@ function beackwalkHotel() {
 
     roomBase = 150;
 
-  } 
+  }
 
   //Discount check
   let discount = 0;
@@ -276,7 +288,7 @@ function beackwalkHotel() {
     discountPrice = (roomPrice / 100) * discount;
 
   }
-  
+
   console.log("");
   console.log("Price for the room: " + roomPrice + "€");
   if (discountPrice != 0) {
@@ -284,7 +296,7 @@ function beackwalkHotel() {
 
   }
 
-  
+
   if (breakfast == "1") {
     breakfastPrice = (nights * 10);
     console.log("Breakfast: " + breakfastPrice + "€");
@@ -304,7 +316,7 @@ function beachWalkHotel_CheckGuests() {
 
   console.log("Number of guests? (1-2)");
   let guests = readlineSync.question("> ");
-  
+
   while (guests != "1" && guests != "2") {
 
     console.clear();
@@ -312,13 +324,13 @@ function beachWalkHotel_CheckGuests() {
     console.log("Number of guests? (1-2)");
 
     guests = readlineSync.question("> ");
-  
+
 
   }
 
   return guests;
 
-  
+
 }
 
 function beachWalkHotel_CheckNights() {
@@ -330,7 +342,7 @@ function beachWalkHotel_CheckNights() {
   while (numCheck == false || parseInt(nights) <= 0) {
 
     console.clear();
-    console.log(nights +" is invalid. Please try again");
+    console.log(nights + " is invalid. Please try again");
     console.log("Number of nights?");
     nights = "";
     nights = readlineSync.question("> ");
@@ -343,20 +355,20 @@ function beachWalkHotel_CheckNights() {
 function beachWalkHotel_CheckBreakfast(guests) {
 
   console.log("Include breakfast for how many guests? 0-" + guests);
-  
+
   let breakfast = readlineSync.question("> ");
   let numCheck = checkStringIsInt(breakfast);
-  
-  while (breakfast > guests || numCheck == false){
+
+  while (breakfast > guests || numCheck == false) {
     console.clear();
-    console.log(breakfast +" is invalid. Please try again");
+    console.log(breakfast + " is invalid. Please try again");
 
     console.log("Include breakfast for how many guests? 0-" + guests);
 
     breakfast = readlineSync.question("> ");
     numCheck = checkStringIsInt(breakfast);
   }
-  
+
   return breakfast;
 }
 
@@ -367,20 +379,20 @@ function beachWalkHotel_CheckRoom() {
   console.log("3. Deluxe suite - 150 per night");
 
   let room = readlineSync.question("> ");
-  
-  while (room != "1" && room != "2" && room != "3"){
+
+  while (room != "1" && room != "2" && room != "3") {
     console.clear();
-    console.log(room +" is invalid. Please try again");
-    
+    console.log(room + " is invalid. Please try again");
+
     console.log("Please choose the room/s ");
     console.log("1. Normal room  - 90€ per night");
     console.log("2. Deluxe room  - 120 per night");
     console.log("3. Deluxe suite - 150 per night");
 
     room = readlineSync.question("> ");
-    
+
   }
-  
+
   return room;
 }
 
@@ -395,7 +407,7 @@ function checkStringIsInt(string) {
 
     // } else {
     //   return false;
-      
+
     // }
     return true;
   } else {
@@ -421,15 +433,15 @@ function calcSum(value) {
 
   let sum = 0;
   let result = "";
-  for (let i = 1; i - 1 < value; i++) {
+  for (let i = 1; i <= value; i++) {
 
     sum = (sum + i);
     result = (result + i + " + ");
 
   }
-  endresult = result.substr(0, result.length - 3)
+  result = result.substr(0, result.length - 3)
 
-  return "Result: " + endresult + " = " + sum;
+  return "Result: " + result + " = " + sum;
 
 }
 
@@ -449,8 +461,132 @@ function calcProduct(value) {
 
 }
 
+//Exercise 8
+function checkIfNumberExists() {
+
+  let numbers = [1, 3, 3, 3, 5, 6, 11, 12, 44, 66, 120, 120, 120, 120, 123, 144, 999, 999];
+
+  console.log("Exercise 8 - Check array for number");
+  console.log("The Array: " + numbers);
+  console.log("");
+  console.log("What number you want to look for?")
+  let num = readlineSync.question("> ");
+
+  counter = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (num == numbers[i]) {
+      counter++;
+    }
+  }
+
+  console.log("You were looking for the number " + num + ".");
+  if (counter > 0) {
+    if (counter > 1) {
+      console.log(num + " exists " + counter + " times in this array.");
+
+    } else {
+      console.log(num + " exists only once in this array.");
+    }
+
+  } else {
+
+    console.log("Sadly that number doesn't exist in this array :(");
+  } 
+  console.log("");
+
+  returnToMenu();
+
+}
+
+//Exercise 9
+function printOddPosition() {
+
+  let names = ["Josie", "Cathrn", "Cathrn", "Max", "Hannah", "Alex"];
+
+  console.log("Exercise 9 - Odd positions");
+  console.log("The Array: " + names);
+  console.log("");
+  for (let i = 0; i < names.length; i++) {
+
+    if ((i % 2) != 0) {
+      console.log(names[i]);
+    }
+  }
+
+  console.log("");
+  returnToMenu();
+}
 
 
+function printLongestName() {
+
+  let names = ["Ute", "Leif", "Erik", "Henry", "Josie", "Helder", "Conner", "Marcel", "Hannah", "Alex"];
+
+  console.log("Exercise 10 - Longest name");
+  console.log("The Array: " + names);
+  console.log("");
+
+  longestName = "";
+  sameLength = "";
+  for (let i = 0; i < names.length; i++) {
+
+    if (names[i].length > longestName.length) {
+      longestName = names[i];
+      sameLength = names[i];
+
+    } else if (names[i].length == longestName.length) {
+      sameLength += (", " + names[i]);
+    }
+  }
+
+
+  if (sameLength.length > longestName.length) {
+    console.log("There are multiple names with the most characters.");
+    console.log("Those are: " + sameLength);
+
+  } else {
+    console.log("The longest name is: " + longestName);
+  }
+
+  console.log("");
+  returnToMenu();
+
+}
+
+function sumAvgHighestLowest() {
+
+  let array = [4, 7, 10, 12, 1, 8, 20, 16];
+  console.log("Exercise 11 - Sum, avg, highest, lowest");
+  console.log("The Array: " + array);
+  console.log("");
+
+  let sum = 0;
+  let highest = 0;
+  let lowest = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+
+    if (array[i] > highest) {
+      highest = array[i];
+    }
+
+    if (array[i] < lowest) {
+      lowest = array[i];
+    }
+  }
+
+  // console.log("Lowest: " + Math.min(...array));
+  // console.log("Highest: " + Math.max(...array));
+  console.log("Lowest: " + lowest);
+  console.log("Highest: " + highest);
+  console.log("Average: " + (sum / array.length));
+  console.log("Sum: " + sum);
+  console.log("");
+
+  returnToMenu();
+
+}
 
 // function endOfFunction(lastFunction){
 
